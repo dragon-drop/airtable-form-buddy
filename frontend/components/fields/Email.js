26bb14b64484869b0e4c5bc9b9b29b70601cb8e6
John Norris <john.norris@johnlewis.co.uk>
2020-07-06 14:45:40 +0100

    Fix bug with Email field not resetting on submission

--- frontend/components/fields/Email.js
+++ frontend/components/fields/Email.js
@@ -1,9 +1,14 @@
 import React, { useState } from 'react';
+import PubSub from 'pubsub-js';
 import { FormField, Input } from "@airtable/blocks/ui";
 import getFieldLabel from '../../helpers/getFieldLabel';
 
 const Email = ({ field, validationConfig }) => {
   const [value, setValue] = useState("");
+
+  PubSub.subscribe('resetForm', () => {
+    setValue('');
+  });
 
   return (
     <FormField label={getFieldLabel(field, validationConfig)}>
